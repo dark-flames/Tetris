cde96a7ea259a5474b814b5f1202371b35b4f0bb
dark-flames <dark_flames@outlook.com>
2020-05-26 03:45:10 +0900

    feat: tetriminos

--- src/tetriminos/tetrimino.py
+++ src/tetriminos/tetrimino.py
@@ -1,22 +1,30 @@
 from abc import ABC, abstractmethod
-from ..internal import Position, Blocks, Self
-from ..config.color import Color
 from copy import deepcopy
+from typing import Tuple
+
+from ..internal import Position, Blocks, Self, Vec, List
+from ..config.color import Color
+
+BlockVectors = Tuple[List[Vec], List[Vec], List[Vec], List[Vec]]
 
 
 class Tetrimino(ABC):
     position: Position
     blocks: Blocks
+    __block_vectors: BlockVectors
+    __status: int
 
     def __init__(self, position: Position) -> None:
         self.position = position
+        self.__status = 0
+        self.__block_vectors = self.get_block_vectors()
         self.update_blocks()
 
     def peek_left(self: Self) -> Self:
         return deepcopy(self).move_left()
 
     def move_left(self: Self) -> Self:
-        self.position[0] -= 1
+        self.position = self.position.left()
         self.update_blocks()
         return self
 
@@ -24,7 +32,7 @@ class Tetrimino(ABC):
         return deepcopy(self).move_right()
 
     def move_right(self) -> Self:
-        self.position[0] += 1
+        self.position = self.position.right()
         self.update_blocks()
         return self.position
 
@@ -32,20 +40,29 @@ class Tetrimino(ABC):
         return deepcopy(self).drop()
 
     def drop(self) -> Self:
-        self.position[1] -= 1
+        self.position = self.position.below()
         self.update_blocks()
         return self.position
 
-    @abstractmethod
     def peek_rotate(self: Self) -> Self:
-        ...
+        return deepcopy(self).rotate()
 
-    @abstractmethod
     def rotate(self: Self) -> Self:
-        ...
+        self.__status = (self.__status + 1) % 4
+        self.update_blocks()
+        return self
+
+    def update_blocks(self: Self) -> Self:
+        self.blocks = list(
+            map(
+                lambda vec: self.position + vec,
+                self.__block_vectors[self.__status]
+            )
+        )
+        return self
 
     @abstractmethod
-    def update_blocks(self: Self) -> Self:
+    def get_block_vectors(self) -> BlockVectors:
         ...
 
     @property
---
+++ src/tetriminos/z_tetrimino.py
@@ -0,0 +1,17 @@
+from .tetrimino import Tetrimino, BlockVectors
+from ..internal import Vec
+from ..config.color import Color
+
+
+class ZTetrimino(Tetrimino):
+    def get_block_vectors(self) -> BlockVectors:
+        return BlockVectors(
+            [Vec(-1, 1), Vec(0, 1), Vec(0, 0), Vec(1, 0)],
+            [Vec(0, 0), Vec(0, 1), Vec(1, 1), Vec(1, 2)],
+            [Vec(-1, 1), Vec(0, 1), Vec(0, 0), Vec(1, 0)],
+            [Vec(0, 0), Vec(0, 1), Vec(1, 1), Vec(1, 2)],
+        )
+
+    @property
+    def color(self) -> Color:
+        return Color.RED
