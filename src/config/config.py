35284d97dc38db8bb702e31658ee0de413b892e1
dark-flames <dark_flames@outlook.com>
2020-05-27 12:02:05 +0900

    feat: parse cli args

--- src/config/config.py
+++ src/config/config.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 
 from internal import Size
+from strategy.random_strategy import RandomStrategy
 from strategy.strategy import Strategy
 from .color import Color
-from .translate import Translate
+from .translate import Translate, translate_factory
 from pyxel import FONT_HEIGHT
+from argparse import ArgumentParser
+from copy import deepcopy
 
 
 @dataclass
@@ -65,3 +68,42 @@ class Config:
 
     def get_speed_ratio(self, level: int) -> float:
         return self.tps_level_accelerate_ratio ** level
+
+    @classmethod
+    def default_config(cls):
+        return Config(
+            dark_mod=False,
+            size=Size([15, 20]),
+            pixel_per_block=10,
+            block_padding=1,
+            strategy=RandomStrategy(),
+            rotate_sampling_interval=2,
+            left_and_right_sampling_interval=2,
+            drop_speed=4,
+            accelerated_drop_ratio=4,
+            default_tick_per_second=8,
+            tps_level_accelerate_ratio=1.1,
+            score_per_line=100,
+            score_per_level=500,
+            window_padding_y=5,
+            window_padding_x=5,
+            center_text_offset_y=40,
+            center_text_margin_y=2,
+            info_text_margin_x=3,
+            info_text_margin_y=1,
+            translate=translate_factory('en')
+        )
+
+    @classmethod
+    def from_args(cls, parser: ArgumentParser):
+        default_config: Config = deepcopy(cls.default_config())
+        parser.add_argument("--dark-mode", action="store_true")
+        parser.add_argument("--size", action="append_const", const=int, default=default_config.size)
+        parser.add_argument("--lang", type=str, default="en")
+
+        args = parser.parse_args()
+        default_config.dark_mod = args.dark_mode
+        default_config.size = Size(args.size)
+        default_config.translate = translate_factory(args.lang)
+
+        return default_config
--- src/main.py
+++ src/main.py
@@ -1,34 +1,15 @@
-from internal import Size
 from config.config import Config
-from config.translate import translate_factory
-from strategy.random_strategy import RandomStrategy
+import argparse
 from core import Core
 
 
 if __name__ == "__main__":
-    core = Core(
-        Config(
-            dark_mod=True,
-            size=Size([15, 20]),
-            pixel_per_block=10,
-            block_padding=1,
-            strategy=RandomStrategy(),
-            rotate_sampling_interval=2,
-            left_and_right_sampling_interval=1,
-            drop_speed=4,
-            accelerated_drop_ratio=4,
-            default_tick_per_second=8,
-            tps_level_accelerate_ratio=1.1,
-            score_per_line=100,
-            score_per_level=500,
-            window_padding_y=5,
-            window_padding_x=5,
-            center_text_offset_y=40,
-            center_text_margin_y=2,
-            info_text_margin_x=3,
-            info_text_margin_y=1,
-            translate=translate_factory('en')
-        )
+    parser = argparse.ArgumentParser(
+        prog='Tetris'
     )
+
+    config: Config = Config.from_args(parser)
+
+    core = Core(config)
 
     core.run()
